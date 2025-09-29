edc2ba8155db32368c5c9b7a8f1d4b5d884a5c94
njr69 <nishiel2003@gmail.com>
2025-09-29 18:29:03 +0530

    added config

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 from test import to_upper, to_lower
 
 if __name__ == "__main__":
-    text = input("Enter some text: ")
+    text = input("devops ia ")
 
     print("Uppercase:", to_upper(text))
     print("Lowercase:", to_lower(text))
