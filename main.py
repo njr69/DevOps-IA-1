1d830d1ead5b7e268e863a34f3a1cb106d935c6d
njr69 <nishiel2003@gmail.com>
2025-09-29 19:40:24 +0530

    changes

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 from test import to_upper, to_lower
 
 if __name__ == "__main__":
-    text = "This is just a test"
+    text = "This is a Ia Recording"
 
     print("Uppercase:", to_upper(text))
     print("Lowercase:", to_lower(text))
