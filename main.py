a66949649ef7f1901034254c426a173f684e591d
njr69 <nishiel2003@gmail.com>
2025-09-29 18:34:03 +0530

    modif

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 from test import to_upper, to_lower
 
 if __name__ == "__main__":
-    text = input("devops ia ")
+    text = "sample value"
 
     print("Uppercase:", to_upper(text))
     print("Lowercase:", to_lower(text))
